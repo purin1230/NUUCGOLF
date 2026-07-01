2a60b992590cbb94df23882b667db1b4a554d042
purin1230 <emma12301230@gmail.com>
2026-07-01 11:06:47 +0800

    Delete config.js

--- config.js
+++
@@ -1,3 +0,0 @@
-// 請將 Google Apps Script 部署後取得的 Web App URL 貼在這裡
-// 格式：https://script.google.com/macros/s/AKfycbxKw7t4a0NG_4xCNeX7pz8Xuip3NG5fJMVeLToDSOE6nn0ZS6jF-fy9Ri8BYqEF2CRSjg/exec
-const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxKw7t4a0NG_4xCNeX7pz8Xuip3NG5fJMVeLToDSOE6nn0ZS6jF-fy9Ri8BYqEF2CRSjg/exec";
